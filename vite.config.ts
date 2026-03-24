cceec02f6526474a6b0806dd754d1a3df8e71aa3
BDRS <arajanemarloro@gmail.com>
2026-03-24 18:25:01 +0800

    Change base path in Vite config to '/website/'

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Replace 'BDRS-insurance' with your exact repo name if it's different
-  base: '/BDRS-insurance/', 
+  base: '/website/', 
 })
